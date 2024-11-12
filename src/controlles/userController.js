380c5d13351515d6ac7156b948385743aa95ba22
Nguyen Phan Thao Nguyen <thaonguyen68161@gmail.com>
2024-11-12 23:53:50 +0700

    login function

--- src/controlles/userController.js
+++ src/controlles/userController.js
@@ -13,6 +13,23 @@ class UserController {
         }
     }
 
+    async loginUser(req, res) {
+        try {
+            const { email, password } = req.body;
+
+            // Kiểm tra dữ liệu đầu vào
+            if (!email || !password) {
+                return res.status(400).json({ error: "Please enter your email and password." });
+            }
+
+            // Gọi hàm login từ userService
+            const result = await userService.loginUser(email, password);
+            res.json(result);
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
+    }
+
 }
 
 module.exports = new UserController();
