2d604d5007b8c4f4007c20bf903cc968d2b4d83e
Nguyễn Thị Quỳnh Mai <147372843+QuynhMai273na@users.noreply.github.com>
2024-11-17 11:16:52 +0700

    Complete full cart function

---
+++ src/services/cartService.js
@@ -0,0 +1,32 @@
+const Cart = require('../models/cart');
+
+class CartService {
+    async addToCart (user, giftId){
+        const newCartItem = new Cart({user, gift: giftId});
+        return await newCartItem.save();
+    }
+
+    async getCartByUser(user){
+        return await Cart.find({user, status: "pending"}).populate("gift");
+    }
+
+    async removeFromCart (cartId) {
+        try {
+            return await Cart.findByIdAndDelete(cartId);
+        } catch (error) {
+            console.error("Error removing cart item:", error.message);
+            throw error;
+        }
+    }
+
+    async getCartById(cartId) {
+        try {
+            const cartItem = await Cart.findById(cartId).populate("gift");
+            return cartItem;
+        }catch(error){
+            console.error("Error fetching cart item by Id: ", error);
+            throw error;
+        }
+    }
+}
+module.exports = new CartService();
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,5 +1,8 @@
 const User = require ("../models/user");
 const jwt = require("jsonwebtoken");
+const path = require('path');
+const dotenv = require('dotenv');
+dotenv.config({ path: path.join(process.cwd(), '.env') });
 
 class UserService {
     async createUser(name, email, password, sex) {
@@ -18,15 +21,27 @@ class UserService {
             throw new Error("Incorrect password.");
         }
 
-        const secret_key="tnguyen";
+        // const secret_key="tnguyen";
+        const secret_key=process.env.JWT_SECRET;
+        
         //const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: "1h" });
-        const token = jwt.sign({ userId: user._id, email: user.email, name: user.name, sex: user.sex, point: user.point, partner:user.partner }, secret_key, { expiresIn: "1h" });
-
+        const token = jwt.sign({ ...user, partner:user.partner }, secret_key, { expiresIn: "1h" });
+        
         return {
             message: "Login success",
             token,
             user: { id: user._id, email: user.email, name: user.name, sex: user.sex, point: user.point, partner:user.partner },
         };
+    }
+    async getUserById(uerId){
+        try{
+            const user= await User.findById(uerId);
+            if(!user) throw new Error("User not found");
+            return user;
+        }catch(error){
+            console.error("Error getting user:", error.message);
+            throw new Error(error.message);
+        }
     }
 };
 module.exports = new UserService();
